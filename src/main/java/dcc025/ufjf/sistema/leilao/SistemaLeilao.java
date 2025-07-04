bc684becf5f56cf0991437a8a9d97ef439ec7947
fernandes-joaopaulo <fernandes.joao@estudante.ufjf.br>
2025-07-04 12:35:20 -0300

    Remove arquivos em desuso

--- src/main/java/dcc025/ufjf/sistema/leilao/SistemaLeilao.java
+++ src/main/java/dcc025/ufjf/sistema/leilao/SistemaLeilao.java
@@ -2,23 +2,12 @@ package dcc025.ufjf.sistema.leilao;
 
 import dcc025.ufjf.interfaces.TelaLogin;
 
-import java.util.Map;
-import java.util.HashMap;
-
 /**
  *
  * @author Joao Paulo
  * Classe principal de execução do sistema
  */
 public class SistemaLeilao {
-
-    private Map<Integer, Usuario> usuarios;
-    private Map<Integer, Leilao> leiloes;
-    
-    public SistemaLeilao(){
-        usuarios = new HashMap<>();
-        leiloes = new HashMap<>();
-    }
     
     public static void main(String[] args) {
         TelaLogin tela = new TelaLogin();
